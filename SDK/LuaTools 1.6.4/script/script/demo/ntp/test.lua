992371894d5282ec4ced8fd6f84a525e69aae93f
hechao <info@electrodragon.com>
2020-04-08 22:13:35 +0800

    update

--- SDK/LuaTools 1.6.4/script/script/demo/ntp/test.lua
+++
@@ -1,8 +0,0 @@
-module(...,package.seeall)
-require"misc"
-
-local function printime()
-	print(misc.getclockstr())
-end
-
-sys.timer_loop_start(printime,1000)
--- SDK/LuaTools 1.6.4/script/script/demo/update/Luat_iot_server/test.lua
+++
@@ -1,3 +0,0 @@
-module(...,package.seeall)
-
-sys.timer_loop_start(print,1000,"test version",_G.VERSION)
